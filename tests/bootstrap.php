7f48331705e7815beaaeba181dc3def17066fc92
Marek Nocoń <mnocon@users.noreply.github.com>
2022-04-19 13:10:08 +0200

    [CS] Fixed CS

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
 $autoloadFile = __DIR__ . '/../vendor/autoload.php';
 if (!file_exists($autoloadFile)) {
     throw new RuntimeException('Install dependencies to run test suite.');
--- tests/lib/Templating/ThemeTemplateNameResolverTest.php
+++ tests/lib/Templating/ThemeTemplateNameResolverTest.php
@@ -1,12 +1,9 @@
 <?php
 
-/*
- * This file is part of the EzPlatformDesignEngine package.
- *
- * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
-
 namespace EzSystems\EzPlatformDesignEngine\Tests\Templating;
 
 use eZ\Publish\Core\MVC\ConfigResolverInterface;
@@ -16,7 +13,7 @@ use PHPUnit\Framework\TestCase;
 class ThemeTemplateNameResolverTest extends TestCase
 {
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject|ConfigResolverInterface
+     * @var \PHPUnit_Framework_MockObject_MockObject|\eZ\Publish\Core\MVC\ConfigResolverInterface
      */
     private $configResolver;
 
